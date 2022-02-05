732c202893080597b0d4d97d5660c673b57eecef
Thiago-DR <thiago.dr.1055@gmail.com>
2022-02-05 17:30:55 -0300

    Game.js working as intended

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
 
-    var gamePattern = []
-    var userClickedPattern = []
+    let gamePattern = []
+    let userClickedPattern = []
+    let level = 0
+    let started = false
 
-    //new color to the sequence
+    //new color to sequence
     function nextSequence() {
         let randomNumber = Math.floor(Math.random() * 4)
         let randomColor
+        userClickedPattern = []
+
+        updateTitle(`Level ${++level}`)
 
         switch (randomNumber) {
             case 0:
@@ -29,22 +34,67 @@ $(document).ready(function () {
 
         gamePattern.push(randomColor)
 
-        $('#' + randomColor).fadeIn(100).fadeOut(50).fadeIn(100)
+        $('#' + randomColor).fadeIn(100).fadeOut(150).fadeIn(100)
         playSound(randomColor)
+
     }
 
+    //update title
+    function updateTitle(title) {
+        $('#level-title').text(title)
+    }
+
+    //compare user pattern with game pattern
+    function checkAnswer(index) {
+        if (userClickedPattern[index - 1] === gamePattern[index - 1]) {
+            if (index === level)
+                setTimeout(nextSequence, 750);
+
+        } else
+            gameOver()
+    }
+
+    //game over
+    function gameOver() {
+        updateTitle('Game Over')
+        $('body').addClass("game-over")
+
+        playSound('wrong')
+
+        setTimeout(reset, 1000)
+    }
+
+    function reset() {
+        updateTitle('Press Any Key to Start')
+        $('body').removeClass("game-over")
+        started = false
+        level = 0
+        gamePattern = []
+    }
     //audio
     function playSound(name) {
         let audio = new Audio('/sounds/' + name + '.mp3')
         audio.play()
     }
 
+    //keyboard listener     
+    $(document).keypress(function () {
+        if (started === false) {
+            nextSequence();
+            started = true
+        }
+    })
+
     //mouse events
     $('.btn').click(function () {
         let userChosenColor = $(this).attr('id')
-        userClickedPattern.push(userChosenColor)
-
         playSound(userChosenColor)
+
+        if (started === true) {
+            userClickedPattern.push(userChosenColor)
+
+            checkAnswer(userClickedPattern.length)
+        }
     });
 
     //button animations
